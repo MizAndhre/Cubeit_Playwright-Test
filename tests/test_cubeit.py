1fa0243b69be2a0e79ed7d8c2604a4b0f86e96a3
Andrea PC Terminal <andry3632@gmail.com>
2025-05-12 23:54:13 -0500

    initial commit

---
+++ tests/test_cubeit.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Page, expect
+
+BASE_URL = "http://127.0.0.1:5500/Cubeit_Playwright-Test/index.html"
+
+def test_cube(page: Page):
+    page.goto(BASE_URL)
+    
+    field = page.get_by_placeholder('enter number...')
+    field.fill('5')
+    
+    btn = page.get_by_role("button", name="Cube")
+    btn.click()
+    
+    result = page.locator("css=p#result")
+    
+    expect(result).to_contain_text("125")
+    
+
+
+def test_empty_input(page: Page):
+    page.goto(BASE_URL)
+    
+    field = page.get_by_placeholder('enter number...')
+    field.fill('')
+    
+    btn = page.get_by_role("button", name="Cube")
+    btn.click()
+    
+    result = page.locator("css=p#result")
+    
+    expect(result).to_have_text("Enter something!")
